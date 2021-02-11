0265063b7d269d195749149ce6f942966ea8e3b4
georgegozal <g2gozal@gmail.com>
2021-02-11 12:04:10 +0100

    second commit

--- main.py
+++ main.py
@@ -11,23 +11,33 @@ result = requests.get("https://www.worldometers.info/coronavirus/country/georgia
 soup = BS(result.text, "lxml")
 
 def corona_statistics():
-    corona_cases=int(soup.select(".maincounter-number  span")[0].getText().replace(",","").replace(" ",""))
-    got_cured = int(soup.select(".maincounter-number span")[2].getText().replace(",","").replace(" ",""))
-    death = int(soup.select(".maincounter-number span")[1].getText().replace(",","").replace(" ",""))
-    active_cases = corona_cases - got_cured - death
-    new_cases= int(soup.select(".news_li strong")[0].getText().split()[0])
-    #new_curred = 1080
-    new_death= int(soup.select(".news_li strong")[1].getText().split()[0])
     highest_daily_case = 5450
     highest_daily_death = 53
     
+    corona_cases=int(soup.select(".maincounter-number  span")[0].getText().replace(",","").replace(" ",""))
+    death = int(soup.select(".maincounter-number span")[1].getText().replace(",","").replace(" ",""))
+    new_death= int(soup.select(".news_li strong")[1].getText().split()[0])
+    
+    new_cases= int(soup.select(".news_li strong")[0].getText().split()[0])
+    got_cured = int(soup.select(".maincounter-number span")[2].getText().replace(",","").replace(" ",""))
+    old_got_cured =0
+    new_curred= 0
+    
+    if new_cases > highest_daily_case:
+        highest_daily_case = new_cases 
+    if new_death > highest_daily_death:
+        highest_daily_death = new_death  
+    if got_cured > old_got_cured:
+        new_curred = got_cured - old_got_cured
+        old_got_cured = got_cured   
+    active_cases = corona_cases - got_cured - death
     print(f"* მთლიანობაში დაღუპულია {death:,} ადამიანი {corona_cases:,} შემთხვევიდან. \nსიკვდილიანობის პროცენტული მაჩვენებელი არის {death / corona_cases :.2%}.")
     print()
     print(f"* გამოჯანმრთელებულია {got_cured:,} ადამიანი რაც არის {got_cured/ (corona_cases) :.2%}." )
     print()
     print(f"* ამჟამად მკურნალობას გადის {active_cases+new_cases:,} დაავადებულების {active_cases/ (corona_cases +new_cases):.2%}.")
     print()
-    print(f"* დღევანდელი მონაცემებით გვაქვს დაინფიცირების {new_cases} შემთხვევა რაც არის {new_cases/highest_daily_case :.2%}\nმაქსიმალური დაინფიცირების მაჩვენებლისა რომელიც იყო {highest_daily_case}, გასული წლის 5 დეკემბერს.")
+    print(f"* დღევანდელი მონაცემებით გვაქვს დაინფიცირების {new_cases} შემთხვევა.")# რაც არის {new_cases/highest_daily_case :.2%}")#\nმაქსიმალური დაინფიცირების მაჩვენებლისა რომელიც იყო {highest_daily_case}, გასული წლის 5 დეკემბერს.")
     print()
     #print(f"* გამოჯანმრთელებულთა რიცხვი უდრის {new_curred}.")
     #print()
