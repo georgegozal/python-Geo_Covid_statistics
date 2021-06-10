9d32ea32f33738eb75896e91650e059fe06877b0
David <Davidchincharashvili@gmail.com>
2021-06-10 19:18:59 +0400

    added function

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import pandas as pd
 from datetime import datetime
 dtime =datetime.now()
 
+def replace(soup, selector, st=0):
+
+    return int(soup.select(selector)[st].getText().split()[0].replace(",",""))
+
+
 def corona_statistics():
     import requests 
     from bs4 import BeautifulSoup as BS
@@ -15,7 +20,10 @@ def corona_statistics():
     wsoup = BS(ge_result.text, features='lxml')
     d='https://www.medalerts.org/vaersdb/findfield.php?EVENTS=on&PAGENO=1&PERPAGE=10&ESORT&REVERSESORT&VAX=(COVID19)&VAXTYPES=(COVID-19)&DIED=Yes&fbclid=IwAR1d3IoDPYk3t1hkUMTY4ArIFK4iiW90CxBXnbarmeMHJlKRq_yPJQcGRwk'
     # colect data
-    new_cases= int(soup.select(".news_li strong")[0].getText().split()[0].replace(",",""))
+
+    new_cases= replace(soup, ".news_li strong", 0)
+
+    # new_cases= int(soup.select(".news_li strong")[0].getText().split()[0].replace(",",""))
     new_deaths= int(soup.select(".news_li strong")[1].getText().split()[0])
     total_deaths = int(soup.select(".maincounter-number span")[1].getText().replace(",","").replace(" ",""))
     total_corona_cases=int(soup.select(".maincounter-number  span")[0].getText().replace(",","").replace(" ",""))
